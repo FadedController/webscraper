d4a2f7e4da7c631c0bc2e6e33ab0dda1c11d3769
Axel Padilla <adpadillar25@gmail.com>
2021-08-31 22:06:01 -0600

    Login to platform

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello from server")
+import puppeteer from "puppeteer";
+import { MY_USER, User } from "./credentials/users";
+
+const LOGIN_URL = "https://plataforma.maristastepic.digital/login/index.php";
+
+const main = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await login(MY_USER, page);
+};
+
+type LoginFx = (u: User, p: puppeteer.Page) => Promise<void>;
+
+const login: LoginFx = async (user, page) => {
+  await page.goto(LOGIN_URL);
+  const loginField = await page.$("#username");
+  const passwordField = await page.$("#password");
+  const submitButton = await page.$("#loginbtn");
+  await loginField?.type(user.username);
+  await passwordField?.type(user.password);
+  await submitButton?.click();
+};
+
+main();
